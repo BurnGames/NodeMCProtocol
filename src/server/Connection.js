8cffa12dd80553df2c4476b1d00d8901cdc67feb
Paul Sauve <paul@burngames.net>
2015-02-04 00:37:18 -0600

    Basics of login

--- src/server/Connection.js
+++ src/server/Connection.js
@@ -1,6 +1,7 @@
 var PacketHandler = require('../packets/PacketHandler');
 var PacketStatus = require('../utils/PacketStatus');
 var PacketReader = require('../utils/PacketReader');
+var crypto = require('crypto');
 
 function Connection(server, socket) {
     var $this = this;
@@ -34,6 +35,7 @@ function Connection(server, socket) {
 }
 
 Connection.prototype.onIncomingPacket = function (packetName, packet) {
+    var status = this.status;
     if (packetName == 'setProtocol') {
         if (packet.nextState == 1) {
             this.status = PacketStatus.STATUS;
@@ -59,13 +61,29 @@ Connection.prototype.onIncomingPacket = function (packetName, packet) {
             }
         };
         this.write(0x00, {response: JSON.stringify(response)});
-    } else if(packetName == 'loginStart') {
-
+    } else if (packetName == 'loginStart') {
+        this.username = packet.username;
+        var serverId = crypto.randomBytes(4).toString('hex');
+        this.verifyToken = crypto.randomBytes(4);
+        var publicKeyArray = this.server.key.toPublicPem('utf8').split('\n');
+        var publicKey = '';
+        for (var i = 0; i < publicKeyArray.length - 2; i++) {
+            publicKey += publicKeyArray[i];
+        }
+        this.publicKey = new Buffer(publicKey, 'base64');
+        var hash = crypto.createHash('sha1');
+        hash.update(serverId);
+        this.write(0x01, {
+            serverId: serverId,
+            publicKey: publicKey,
+            verifyToken: this.verifyToken
+        })
     }
     else if (packetName == 'ping') {
         this.write(0x01, {ping: packet.ping});
     }
     console.log(packetName + ": " + JSON.stringify(packet));
+    console.log('Changed status from ' + status + ' to ' + this.status);
 };
 
 Connection.prototype.write = function (packetId, params) {
